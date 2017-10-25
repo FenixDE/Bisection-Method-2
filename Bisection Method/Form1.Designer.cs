2fa2470c866489b920c392cf033fb58e32e84e33
fenix <fenix@DESKTOP-R42RPUM>
2017-10-25 14:52:40 +0600

    изменение

--- Bisection Method/Form1.Designer.cs
+++ Bisection Method/Form1.Designer.cs
@@ -231,9 +231,9 @@
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Font = new System.Drawing.Font("Monotype Corsiva", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.button1.Location = new System.Drawing.Point(465, 38);
+            this.button1.Location = new System.Drawing.Point(465, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(162, 68);
+            this.button1.Size = new System.Drawing.Size(162, 94);
             this.button1.TabIndex = 19;
             this.button1.Text = "Calculate";
             this.button1.UseVisualStyleBackColor = false;
